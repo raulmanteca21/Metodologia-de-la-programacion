fc5c8725a58e36a51f7884477d7b9f14ff724af0
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-16 20:17:39 +0100

    Ampliacion punto movil , ejercicio punto2D, 3D

---
+++ Test/Java/Practica3_HerenciaDeClases/Point2D_Point3D/MovablePointTest.java
@@ -0,0 +1,40 @@
+package Practica3_HerenciaDeClases.Point2D_Point3D;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovablePointTest {
+
+    @Test
+    void getXSpeed() {
+    }
+
+    @Test
+    void setXSpeed() {
+    }
+
+    @Test
+    void getYSpeed() {
+    }
+
+    @Test
+    void setYSpeed() {
+    }
+
+    @Test
+    void setSpeed() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void move() {
+    }
+}
--- src/Practica3_HerenciaDeClases/Point2D_Point3D/TestPoint.java
+++ src/Practica3_HerenciaDeClases/Point2D_Point3D/TestPoint.java
@@ -12,10 +12,10 @@ public class TestPoint {
 
         p1.setX(6.0f);
         p1.setY(8.0f);
-        System.out.println("After setX/setY: " + p1);
+        System.out.println("Despues de setX/setY: " + p1);
 
         p1.setXY(1.0f, 2.0f);
-        System.out.println("After setXY: " + p1);
+        System.out.println("Despues de setXY: " + p1);
 
         float[] xy = p1.getXY();
         System.out.println("getXY(): (" + xy[0] + "," + xy[1] + ")");
@@ -30,12 +30,42 @@ public class TestPoint {
         System.out.println("Z: " + p2.getZ());
 
         p2.setZ(10.0f);
-        System.out.println("After setZ: " + p2);
+        System.out.println("Despues de setZ: " + p2);
 
         p2.setXYZ(1.0f, 2.0f, 3.0f);
-        System.out.println("After setXYZ: " + p2);
+        System.out.println("Despues de setXYZ: " + p2);
 
         float[] xyz = p2.getXYZ();
         System.out.println("getXYZ(): (" + xyz[0] + "," + xyz[1] + "," + xyz[2] + ")");
+
+
+
+
+        // ===== Test MovablePoint =====
+        MovablePoint mp1 = new MovablePoint(1.0f, 2.0f, 0.5f, 1.5f);
+
+        System.out.println(mp1);                 // Test toString()
+
+        System.out.println("x es: " + mp1.getX());
+        System.out.println("y es: " + mp1.getY());
+
+        System.out.println("xSpeed es: " + mp1.getXSpeed());
+        System.out.println("ySpeed es: " + mp1.getYSpeed());
+
+// Test getSpeed()
+        float[] speed = mp1.getSpeed();
+        System.out.println("Speed es: (" + speed[0] + "," + speed[1] + ")");
+
+// Test setSpeed()
+        mp1.setSpeed(2.0f, 3.0f);
+        System.out.println(mp1);
+
+// Test move()
+        mp1.move();
+        System.out.println("Tras move: " + mp1);
+
+// Test move varias veces
+        mp1.move().move();
+        System.out.println("Tras dos move: " + mp1);
     }
 }
