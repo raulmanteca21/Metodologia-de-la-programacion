4089d21eaab69fb93f686440a10e6c105a5b8f00
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-16 17:55:17 +0100

    Import con junit sin usar

--- Test/Java/Practica2_ComposicionAgregacion/CustomerBill/TestCustomerTest.java
+++ Test/Java/Practica2_ComposicionAgregacion/CustomerBill/TestCustomerTest.java
@@ -2,8 +2,6 @@ package Practica2_ComposicionAgregacion.CustomerBill;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TestCustomerTest {
 
     @Test
