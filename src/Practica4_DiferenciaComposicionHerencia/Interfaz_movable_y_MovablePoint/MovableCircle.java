a0bfc720a3002ce6f02f8123bde56251398270f6
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-17 13:56:26 +0100

    Actualizacion del movable point para el 6.4

---
+++ src/Practica4_DiferenciaComposicionHerencia/Interfaz_movable_y_MovablePoint/MovableCircle.java
@@ -0,0 +1,37 @@
+package Practica4_DiferenciaComposicionHerencia.Interfaz_movable_y_MovablePoint;
+
+public class MovableCircle implements Movable {
+
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return center.toString() + ",radius=" + radius;
+    }
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+}
--- src/Practica4_DiferenciaComposicionHerencia/Interfaz_movable_y_MovablePoint/TestMovablePoint.java
+++ src/Practica4_DiferenciaComposicionHerencia/Interfaz_movable_y_MovablePoint/TestMovablePoint.java
@@ -26,5 +26,26 @@ public class TestMovablePoint {
         p1.moveRight();
         p1.moveDown();
         System.out.println("Despues de  moveRight hacemos moveDown: " + p1);
+
+        //Test para la nueva clase MovableCircle
+        MovableCircle c1 = new MovableCircle(10, 10, 1, 2, 5);
+
+        System.out.println(c1);
+
+        c1.moveUp();
+        System.out.println("Despues de moveUp: " + c1);
+
+        c1.moveDown();
+        System.out.println("Despues de moveDown: " + c1);
+
+        c1.moveLeft();
+        System.out.println("Despues de moveLeft: " + c1);
+
+        c1.moveRight();
+        System.out.println("Despues de moveRight: " + c1);
+
+        c1.moveRight();
+        c1.moveDown();
+        System.out.println("despues de moveRight y moveDown: " + c1);
     }
 }
