deaef5672ee0f0e35e3b1b43422bc5787366e7ac
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-17 20:33:43 +0100

    Test Account cuaderno 1 vacios

---
+++ Test/Java/Practica1_Clases/Account/AccountTest.java
@@ -0,0 +1,36 @@
+package Practica1_Clases.Account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void credit() {
+    }
+
+    @Test
+    void debit() {
+    }
+
+    @Test
+    void transferTo() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ Test/Java/Practica1_Clases/Account/AccountTesterTest.java
@@ -0,0 +1,12 @@
+package Practica1_Clases.Account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTesterTest {
+
+    @Test
+    void main() {
+    }
+}
