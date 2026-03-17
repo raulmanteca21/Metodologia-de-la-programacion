1f1a166556c1df3e8f14c43c943b0eeb0f74898a
Izan <izan.sanchezg@edu.uah.es>
2026-03-17 22:55:45 +0100

    Añadidos los test para el ejercicio 3.2

--- Test/Java/Practica3_HerenciaDeClases/Person_y_subclases/PersonTest.java
+++ Test/Java/Practica3_HerenciaDeClases/Person_y_subclases/PersonTest.java
@@ -1,24 +1,30 @@
 package Practica3_HerenciaDeClases.Person_y_subclases;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
     @Test
-    void getAddress() {
+    void testGetNameAndAddress() {
+        Person p = new Person("John", "Madrid");
+        assertEquals("John", p.getName());
+        assertEquals("Madrid", p.getAddress());
     }
 
     @Test
-    void getName() {
-    }
-
-    @Test
-    void setAddress() {
+    void testSetAddress() {
+        Person p = new Person("John", "Madrid");
+        p.setAddress("Barcelona");
+        assertEquals("Barcelona", p.getAddress());
     }
 
     @Test
     void testToString() {
+        Person p = new Person("John", "Madrid");
+        assertEquals("Person[name= John,address=Madrid]", p.toString());
+
+        p.setAddress("Barcelona");
+        assertEquals("Person[name= John,address=Barcelona]", p.toString());
     }
 }
