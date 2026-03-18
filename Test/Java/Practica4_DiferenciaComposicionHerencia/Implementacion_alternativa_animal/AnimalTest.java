da088ada1c5d81489e93b92fee035d4aa31f97fa
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-18 13:03:29 +0100

    Test Implementacion alternativa animal, añadiendo esto al import por abstract : package Practica4_DiferenciaComposicionHerencia.Implementacion_alternativa_animal;
                                                                                    import java.io.ByteArrayOutputStream;
                                                                                    import java.io.PrintStream;

--- Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/AnimalTest.java
+++ Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/AnimalTest.java
@@ -2,11 +2,24 @@ package Practica4_DiferenciaComposicionHerencia.Implementacion_alternativa_anima
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
 
     @Test
     void greeting() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
+
+        Animal animal = new Cat(); // usamos una subclase
+        animal.greeting();
+
+        System.setOut(originalOut);
+
+        assertEquals("Meow!" + System.lineSeparator(), output.toString());
     }
 }
--- Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/DogTest.java
+++ Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/DogTest.java
@@ -2,15 +2,39 @@ package Practica4_DiferenciaComposicionHerencia.Implementacion_alternativa_anima
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DogTest {
 
     @Test
     void greeting() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
+
+        Dog dog = new Dog();
+        dog.greeting();
+
+        System.setOut(originalOut);
+
+        assertEquals("Woof!" + System.lineSeparator(), output.toString());
     }
 
     @Test
     void testGreeting() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        dog1.greeting(dog2);
+
+        System.setOut(originalOut);
+
+        assertEquals("Woooooooooof!" + System.lineSeparator(), output.toString());
     }
 }
