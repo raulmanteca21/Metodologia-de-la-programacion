c8cb4e16dd00c06541b7329802e0eacfe894cee3
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-18 13:03:29 +0100

    Test Implementacion alternativa animal, añadiendo esto al import por abstract : package Practica4_DiferenciaComposicionHerencia.Implementacion_alternativa_animal;
                                                                                    import org.junit.jupiter.api.Test;
    
                                                                                    import java.io.ByteArrayOutputStream;
                                                                                    import java.io.PrintStream;
    
                                                                                    import static org.junit.jupiter.api.Assertions.*;
    
                                                                                    class TestAnimalTest {
    
                                                                                        @Test
                                                                                        void main() {
                                                                                            ByteArrayOutputStream output = new ByteArrayOutputStream();
                                                                                            PrintStream originalOut = System.out;
                                                                                            System.setOut(new PrintStream(output));
    
                                                                                            TestAnimal.main(new String[]{});
    
                                                                                            System.setOut(originalOut);
    
                                                                                            String result = output.toString();
    
                                                                                            assertTrue(result.contains("Meow!"));
                                                                                            assertTrue(result.contains("Woof!"));
                                                                                            assertTrue(result.contains("Woow!"));
                                                                                            assertTrue(result.contains("Woooooooooof!"));
                                                                                            assertTrue(result.contains("Woooooowwwww!"));
                                                                                        }
                                                                                    }

--- Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/BigDogTest.java
+++ Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/BigDogTest.java
@@ -2,15 +2,39 @@ package Practica4_DiferenciaComposicionHerencia.Implementacion_alternativa_anima
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BigDogTest {
 
     @Test
     void greeting() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
+
+        BigDog bigDog = new BigDog();
+        bigDog.greeting();
+
+        System.setOut(originalOut);
+
+        assertEquals("Woow!" + System.lineSeparator(), output.toString());
     }
 
     @Test
     void testGreeting() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
+
+        BigDog bigDog = new BigDog();
+        Dog dog = new Dog();
+        bigDog.greeting(dog);
+
+        System.setOut(originalOut);
+
+        assertEquals("Woooooowwwww!" + System.lineSeparator(), output.toString());
     }
 }
