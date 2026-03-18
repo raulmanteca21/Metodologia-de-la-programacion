cc38c03e2d870514ef80982d5976e12ae442102c
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-18 13:10:11 +0100

    Commit definitivos test animal implementacion, se añaden estas dos lineas por abstract import java.io.ByteArrayOutputStream;                                                                                        import java.io.PrintStream;

--- Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/TestAnimalTest.java
+++ Test/Java/Practica4_DiferenciaComposicionHerencia/Implementacion_alternativa_animal/TestAnimalTest.java
@@ -2,11 +2,29 @@ package Practica4_DiferenciaComposicionHerencia.Implementacion_alternativa_anima
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestAnimalTest {
 
     @Test
     void main() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
+
+        TestAnimal.main(new String[]{});
+
+        System.setOut(originalOut);
+
+        String result = output.toString();
+
+        assertTrue(result.contains("Meow!"));
+        assertTrue(result.contains("Woof!"));
+        assertTrue(result.contains("Woow!"));
+        assertTrue(result.contains("Woooooooooof!"));
+        assertTrue(result.contains("Woooooowwwww!"));
     }
 }
