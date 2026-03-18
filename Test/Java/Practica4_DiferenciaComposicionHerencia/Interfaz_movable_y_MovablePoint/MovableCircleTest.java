30f0d0109f81a72a543be639c809ff15e0e99597
Raul Manteca <raul.manteca@edu.uah.es>
2026-03-18 13:34:54 +0100

    Test interfaz movable y movable point cuaderno 4

--- Test/Java/Practica4_DiferenciaComposicionHerencia/Interfaz_movable_y_MovablePoint/MovableCircleTest.java
+++ Test/Java/Practica4_DiferenciaComposicionHerencia/Interfaz_movable_y_MovablePoint/MovableCircleTest.java
@@ -8,21 +8,40 @@ class MovableCircleTest {
 
     @Test
     void testToString() {
+        MovableCircle circle = new MovableCircle(10, 10, 1, 2, 5);
+
+        assertEquals("MovablePoint[x=10,y=10,xSpeed=1,ySpeed=2],radius=5", circle.toString());
     }
 
     @Test
     void moveUp() {
+        MovableCircle circle = new MovableCircle(10, 10, 1, 2, 5);
+        circle.moveUp();
+
+        assertEquals("MovablePoint[x=10,y=8,xSpeed=1,ySpeed=2],radius=5", circle.toString());
     }
 
     @Test
     void moveDown() {
+        MovableCircle circle = new MovableCircle(10, 10, 1, 2, 5);
+        circle.moveDown();
+
+        assertEquals("MovablePoint[x=10,y=12,xSpeed=1,ySpeed=2],radius=5", circle.toString());
     }
 
     @Test
     void moveLeft() {
+        MovableCircle circle = new MovableCircle(10, 10, 1, 2, 5);
+        circle.moveLeft();
+
+        assertEquals("MovablePoint[x=9,y=10,xSpeed=1,ySpeed=2],radius=5", circle.toString());
     }
 
     @Test
     void moveRight() {
+        MovableCircle circle = new MovableCircle(10, 10, 1, 2, 5);
+        circle.moveRight();
+
+        assertEquals("MovablePoint[x=11,y=10,xSpeed=1,ySpeed=2],radius=5", circle.toString());
     }
 }
--- Test/Java/Practica4_DiferenciaComposicionHerencia/Interfaz_movable_y_MovablePoint/MovablePointTest.java
+++ Test/Java/Practica4_DiferenciaComposicionHerencia/Interfaz_movable_y_MovablePoint/MovablePointTest.java
@@ -8,21 +8,40 @@ class MovablePointTest {
 
     @Test
     void moveUp() {
+        MovablePoint point = new MovablePoint(5, 5, 2, 3);
+        point.moveUp();
+
+        assertEquals("MovablePoint[x=5,y=2,xSpeed=2,ySpeed=3]", point.toString());
     }
 
     @Test
     void moveDown() {
+        MovablePoint point = new MovablePoint(5, 5, 2, 3);
+        point.moveDown();
+
+        assertEquals("MovablePoint[x=5,y=8,xSpeed=2,ySpeed=3]", point.toString());
     }
 
     @Test
     void moveLeft() {
+        MovablePoint point = new MovablePoint(5, 5, 2, 3);
+        point.moveLeft();
+
+        assertEquals("MovablePoint[x=3,y=5,xSpeed=2,ySpeed=3]", point.toString());
     }
 
     @Test
     void moveRight() {
+        MovablePoint point = new MovablePoint(5, 5, 2, 3);
+        point.moveRight();
+
+        assertEquals("MovablePoint[x=7,y=5,xSpeed=2,ySpeed=3]", point.toString());
     }
 
     @Test
     void testToString() {
+        MovablePoint point = new MovablePoint(5, 5, 2, 3);
+
+        assertEquals("MovablePoint[x=5,y=5,xSpeed=2,ySpeed=3]", point.toString());
     }
 }
